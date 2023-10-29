9a75b932f43b03435cbe05aebef194ddddc1ee63
michael86 <michael8t6@gmail.com>
2023-10-29 16:02:59 +0000

    added edgecase checks for location searches and api status issues

--- src/features/search_field/SearchField.tsx
+++ src/features/search_field/SearchField.tsx
@@ -1,7 +1,5 @@
-import axios from "axios";
 import { useRef, useState } from "react";
-
-import { GeoCodeData, GeoCodeRes } from "../../interfaces";
+import { GeoCodeData } from "../../interfaces";
 import SearchCards from "./SearchCards";
 import "./search.css";
 import { getGeoCoords } from "./utils";
@@ -9,23 +7,37 @@ import { getGeoCoords } from "./utils";
 const SearchField = () => {
   const inputRef = useRef<HTMLInputElement>(null);
   const [searchLocations, setSearchLocations] = useState<GeoCodeData[]>();
+  const [warning, setWarning] = useState<string | null>(null);
 
   const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (!inputRef.current?.value.length) return;
+    setWarning(null);
+
+    if (!inputRef.current?.value.length) {
+      setWarning("invalid location");
+      return;
+    }
 
     const validLocations = await getGeoCoords(inputRef.current?.value);
 
-    if (typeof validLocations === "string") return;
+    if (typeof validLocations === "string") {
+      validLocations === "status"
+        ? setWarning("there seems to be an issue our end, please hold tight and try again later")
+        : setWarning("That locations wasn't found, are you sure it exists");
+      return;
+    }
 
     setSearchLocations(validLocations);
   };
 
   return (
     <form onSubmit={onSubmit}>
-      <input type={"text"} placeholder="Search for town or city" ref={inputRef} />
-      <button type="submit">Search</button>
+      <div className="input-container">
+        <input type={"text"} placeholder="Search for town or city" ref={inputRef} />
+        <button type="submit">Search</button>
+      </div>
+      {warning && <h3 className="input-warning">{warning}</h3>}
 
       {searchLocations && (
         <div className="search-location--container">
